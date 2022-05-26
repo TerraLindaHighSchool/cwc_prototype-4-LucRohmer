ad0575d063e26e86e4736929d39aa6ca608548c0
LucRohmer <89489290+LucRohmer@users.noreply.github.com>
2022-05-26 14:35:54 -0700

    Revert "Worked on Game Over"
    This reverts commit cd87bc0d443f1db77b42a6912d21501a77366ed0.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
